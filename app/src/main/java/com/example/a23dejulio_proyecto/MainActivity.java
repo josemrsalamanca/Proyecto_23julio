a4a92cadebdd4fa37160d09a4101fb8e70e9cabb
josemrsalamanca <josemrsalamanca@gmail.com>
2021-07-24 12:58:03 -0400

    cambiar

--- app/src/main/java/com/example/a23dejulio_proyecto/MainActivity.java
+++ app/src/main/java/com/example/a23dejulio_proyecto/MainActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tv1 = findViewById(R.id.tv1);
     }
 }
