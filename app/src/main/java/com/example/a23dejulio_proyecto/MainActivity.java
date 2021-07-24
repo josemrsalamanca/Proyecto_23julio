ff0d33ca623e876aa689fb24cce09b9c0f460cde
josemrsalamanca <josemrsalamanca@gmail.com>
2021-07-24 12:51:43 -0400

    modificacion

--- app/src/main/java/com/example/a23dejulio_proyecto/MainActivity.java
+++ app/src/main/java/com/example/a23dejulio_proyecto/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.a23dejulio_proyecto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tv1 = findViewById(R.id.tv1);
+
+        // wjmkkmklds
     }
 }
